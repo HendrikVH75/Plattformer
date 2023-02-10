d178b80f18713278116ae4d63b44748d0527c000
HendrikVH75 <hendrikvh@web.de>
2023-02-10 12:13:28 +0100

    Pause overlay and menu continued

--- src/gamestates/Playing.java
+++ src/gamestates/Playing.java
@@ -3,6 +3,7 @@ package gamestates;
 import entities.Player;
 import levels.LevelManager;
 import main.Game;
+import ui.PauseOverlay;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
@@ -12,6 +13,7 @@ public class Playing extends State implements Statemethods {
 
     private Player player;
     private LevelManager levelManager;
+    private PauseOverlay pauseOverlay;
     private boolean paused;
 
     public Playing(Game game) {
@@ -24,6 +26,7 @@ public class Playing extends State implements Statemethods {
         levelManager = new LevelManager(game);
         player = new Player(200, 200, (int) (64 * Game.SCALE), (int) (40 * Game.SCALE));
         player.loadLvlData(levelManager.getCurrentLevel().getLvlData());
+        pauseOverlay = new PauseOverlay();
 
     }
 
@@ -46,6 +49,7 @@ public class Playing extends State implements Statemethods {
     public void draw(Graphics g) {
         levelManager.draw(g);
         player.render(g);
+        pauseOverlay.draw(g);
     }
 
     @Override
@@ -84,6 +88,9 @@ public class Playing extends State implements Statemethods {
                 break;
             case KeyEvent.VK_BACK_SPACE:
                 Gamestate.state = Gamestate.MENU;
+                break;
+            case KeyEvent.VK_ESCAPE:
+                System.exit(0);
                 break;
             default:
                 break;
--- src/ui/PauseOverlay.java
+++ src/ui/PauseOverlay.java
@@ -1,24 +1,66 @@
 package ui;
 
+import main.Game;
+import utilz.LoadSave;
+
 import java.awt.*;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+
+import static utilz.Constants.UI.PauseButtons.SOUND_SIZE;
 
 public class PauseOverlay {
 
-    public PauseOverlay(){
+    private BufferedImage backgroundImg;
+    private int bgX, bgY, bgW, bgH;
+    private SoundButton musicButton, sfxButton;
+
+    public PauseOverlay() {
+
+        loadBackground();
+        createSoundButtons();
 
     }
 
-    public void update(){
+    private void createSoundButtons() {
+
+        int soundX = (int) (450 * Game.SCALE);
+        int musicY = (int) (140 * Game.SCALE);
+        int sfxY = (int) (186 * Game.SCALE);
+        musicButton = new SoundButton(soundX, musicY, SOUND_SIZE, SOUND_SIZE);
+        sfxButton = new SoundButton(soundX, sfxY, SOUND_SIZE, SOUND_SIZE);
 
     }
 
-    public void draw(Graphics g){
+    private void loadBackground() {
 
+        backgroundImg = LoadSave.getSpriteAtlas(LoadSave.PAUSE_BACKGROUND);
+        bgW = (int) (backgroundImg.getWidth() * Game.SCALE);
+        bgH = (int) (backgroundImg.getHeight() * Game.SCALE);
+        bgX = (Game.GAME_WIDTH / 2 - bgW / 2);
+        bgY = (int) (25 * Game.SCALE);
+
+    }
+
+    public void update() {
+
+    }
+
+    public void draw(Graphics g) {
+        // Background image
+        g.drawImage(backgroundImg, bgX, bgY, bgW, bgH, null);
+
+        // Button images
+        musicButton.draw(g);
+        sfxButton.draw(g);
     }
 
     public void mousePressed(MouseEvent e) {
-
+        if (isIn(e, musicButton)) {
+            musicButton.setMousePressed(true);
+        } else if (isIn(e, sfxButton)) {
+            sfxButton.setMousePressed(true);
+        }
     }
 
     public void mouseReleased(MouseEvent e) {
@@ -29,7 +71,11 @@ public class PauseOverlay {
 
     }
 
-    public void mouseDragged(MouseEvent e){
+    public void mouseDragged(MouseEvent e) {
 
+    }
+
+    private boolean isIn(MouseEvent e, PauseButton b) {
+        return b.getBounds().contains(e.getX(), e.getY());
     }
 }
